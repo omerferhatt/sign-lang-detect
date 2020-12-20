875daf9f8d3e472ecbca27034cef8415db0c274a
omerferhatt <omerf.sarioglu@gmail.com>
2020-12-20 06:14:00 +0300

    Code update - last changes

--- models/model.py
+++ models/model.py
@@ -1,19 +1,26 @@
 import typing
 
+import tensorflow as tf
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, Dropout, ReLU, GlobalAvgPool2D, Input
+from tensorflow.keras.layers import Dense, Dropout, ReLU, GlobalAvgPool2D, Input, Conv2D, BatchNormalization
 
 from models.backbone import Backbone
 
 
 class DenseModel(Backbone):
     def __init__(self,
+                 optimizer,
+                 loss,
+                 metrics,
                  num_hidden_units: tuple,
                  backbone_name: str,
                  input_shape: tuple,
                  backbone_weights: typing.Union[None, str],
                  backbone_trainable=False):
         super(DenseModel, self).__init__(backbone_name, input_shape, backbone_weights, backbone_trainable)
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics
         self.num_hidden_units = num_hidden_units
 
         self.top = self.top_model(top_input=self.backbone.output_shape)
@@ -21,6 +28,8 @@ class DenseModel(Backbone):
             self.backbone,
             self.top,
         ])
+
+        self.model.compile(self.optimizer, self.loss, self.metrics)
 
     @staticmethod
     def dense_block(x, units,  drop_rate=0.2):
@@ -39,10 +48,51 @@ class DenseModel(Backbone):
         model = Model(inputs=x_inp, outputs=x_out, name=self.backbone_name + '_top')
         return model
 
+    def set_freeze(self, block_select, opt, loss, metrics):
+        conv_layer_idx = [(idx, layer.name, layer.count_params()) for idx, layer in enumerate(self.model.layers[0].layers) if isinstance(layer, Conv2D)]
+        if block_select == 0:
+            for layer in self.model.layers[0].layers:
+                layer.trainable = False
+        elif block_select == -1:
+            for layer in self.model.layers[0].layers:
+                layer.trainable = True
+        else:
+            block_select = len(conv_layer_idx) - block_select + 1
+            if block_select > len(conv_layer_idx):
+                print('You passed max block count, max:', len(conv_layer_idx))
+            else:
+                for idx, layer in enumerate(self.model.layers[0].layers):
+                    if idx < conv_layer_idx[block_select - 1][0]:
+                        layer.trainable = False
+                    else:
+                        if isinstance(layer, BatchNormalization):
+                            layer.trainable = False
+                        else:
+                            layer.trainable = True
+
+        if opt is None:
+            opt = self.optimizer
+        if loss is None:
+            loss = self.loss
+        if metrics is None:
+            metrics = self.metrics
+        self.model.compile(opt, loss, metrics)
+
 
 if __name__ == '__main__':
+    opt = tf.keras.optimizers.Adam(learning_rate=0.02)
+    loss = tf.keras.losses.CategoricalCrossentropy()
+    metrics = [tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.TopKCategoricalAccuracy(k=3)]
+
     dm = DenseModel(
+        optimizer=opt,
+        loss=loss,
+        metrics=metrics,
         num_hidden_units=(512,),
-        backbone_name='densenet121',
+        backbone_name='effnetb0',
         input_shape=(224, 224, 3),
-        backbone_weights='imagenet')
+        backbone_weights='imagenet',
+        backbone_trainable=True)
+
+    dm.set_freeze(0)
+    dm.model.summary()
