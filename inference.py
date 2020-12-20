a2dcce86ba6f3771fa1b4c8ec3ad3d798fde0cb7
omerferhatt <omerf.sarioglu@gmail.com>
2020-12-20 08:12:40 +0300

    Code update - last changes

--- inference.py
+++ inference.py
@@ -1,4 +1,44 @@
+import os
+import numpy as np
+import tensorflow as tf
 import cv2
 
 
+labels = sorted(os.listdir('data/Train'))
+
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+input_shape = input_details[0]['shape']
+
+
 cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_resized = cv2.resize(rgb, (224, 224))
+    normalized = rgb_resized[np.newaxis, :, :, :].astype(np.float32)
+    interpreter.set_tensor(input_details[0]['index'], normalized)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    if np.max(output_data.squeeze()) > 0.4:
+        print(labels[np.argmax(output_data).squeeze()], np.max(output_data))
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
