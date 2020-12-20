84c5c83399b1c7bc525f13229cef3439368d5983
Omer Ferhat Sarioglu <omerf.sarioglu@gmail.com>
2020-12-20 05:59:17 +0300

    -

--- inference.py
+++ inference.py
@@ -4,18 +4,18 @@ import tensorflow as tf
 import cv2
 
 
-labels = sorted(os.listdir('data/Train'))
-labels_custom = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Y', 'Z', 'Z_DEL', 'Z_SPACE']
+# labels = sorted(os.listdir('data/Train'))
+labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Y', 'Z', 'Z_DEL', 'Z_SPACE']
 interpreter = tf.lite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 input_shape = input_details[0]['shape']
-
+word_list = ''
 
 cap = cv2.VideoCapture(0)
-
+temp_list = []
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -33,12 +33,27 @@ while True:
     interpreter.set_tensor(input_details[0]['index'], normalized)
     interpreter.invoke()
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    if np.max(output_data.squeeze()) > 0.4:
-        print(labels[np.argmax(output_data).squeeze()], np.max(output_data))
+    frame = cv2.putText(frame, word_list,
+                        (30, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+    if np.max(output_data.squeeze()) > 0.30:
+        frame = cv2.putText(frame, str(labels[np.argmax(output_data).squeeze()]) + ' ' + str(np.max(output_data)),
+                            (30, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        temp_list.append(labels[np.argmax(output_data).squeeze()])
+        if len(list(set(temp_list[-15:]))) == 1 and len(temp_list) > 15:
+            if temp_list[-1] == 'Z_SPACE':
+                word_list += '_'
+            elif temp_list[-1] == 'Z_DEL':
+                word_list = word_list[:-1]
+            else:
+                word_list += temp_list[-1]
+            temp_list = []
+            # print(word_list)
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
+    elif cv2.waitKey(1) == ord('c'):
+        word_list = ''
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
