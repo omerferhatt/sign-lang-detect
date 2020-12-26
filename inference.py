44d1085bb62fd0b36293f5aae5304f4ad2325294
omerferhatt <omerf.sarioglu@gmail.com>
2020-12-26 16:53:26 +0300

    checkpoint deleted

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ import cv2
 
 # labels = sorted(os.listdir('data/Train'))
 labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Y', 'Z', 'Z_DEL', 'Z_SPACE']
-interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter = tf.lite.Interpreter(model_path="saved_models/model_mobilenetv3_aug_86.tflite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
@@ -14,38 +14,52 @@ output_details = interpreter.get_output_details()
 input_shape = input_details[0]['shape']
 word_list = ''
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("http://192.168.1.24:3000/?action=stream")
 temp_list = []
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+
+_, im = cap.read()
+r = cv2.selectROI(im)
+f = open('test.txt', 'a+')
+f.write("\n")
+f.close()
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb = rgb[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
     rgb_resized = cv2.resize(rgb, (224, 224))
     normalized = rgb_resized[np.newaxis, :, :, :].astype(np.float32)
     interpreter.set_tensor(input_details[0]['index'], normalized)
     interpreter.invoke()
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    frame = cv2.putText(frame, word_list,
-                        (30, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+    frame_new = cv2.rectangle(frame, pt1=(r[0], r[1]), pt2=(r[0]+r[2], r[1]+r[3]), color=(0, 0, 255), thickness=1)
+    frame_new = cv2.putText(frame_new, word_list,
+                            (30, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
     if np.max(output_data.squeeze()) > 0.30:
         frame = cv2.putText(frame, str(labels[np.argmax(output_data).squeeze()]) + ' ' + str(np.max(output_data)),
                             (30, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         temp_list.append(labels[np.argmax(output_data).squeeze()])
         if len(list(set(temp_list[-15:]))) == 1 and len(temp_list) > 15:
+            f = open('test.txt', 'a+')
             if temp_list[-1] == 'Z_SPACE':
-                word_list += '_'
+                word_list += ' '
+                f.write(temp_list[-1])
             elif temp_list[-1] == 'Z_DEL':
                 word_list = word_list[:-1]
+                f.write(temp_list[-1])
             else:
                 word_list += temp_list[-1]
+                f.write(temp_list[-1])
+            f.close()
             temp_list = []
             # print(word_list)
     # Display the resulting frame
--- utils/augmentation.py
+++ utils/augmentation.py
@@ -1,11 +1,38 @@
+import numpy as np
 import cv2
 import albumentations as A
 
 
 train_transforms = A.Compose([
+    A.Rotate((-20, 20), always_apply=True),
     A.HorizontalFlip(p=0.5),
-#     A.Rotate(limit=20, p=1., interpolation=cv2.INTER_LINEAR),
-#     A.ElasticTransform(alpha=1.0, sigma=20., alpha_affine=20.),
-#     A.Downscale(scale_min=0.7, scale_max=0.99),
-#     A.MotionBlur(blur_limit=15),
+    A.OneOf([
+        A.IAAAdditiveGaussianNoise(),
+        A.GaussNoise(),
+    ], p=0.2),
+    A.OneOf([
+        A.MotionBlur(p=0.2),
+        A.MedianBlur(blur_limit=3, p=0.1),
+        A.Blur(blur_limit=3, p=0.1),
+    ], p=0.2),
+    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
+    A.OneOf([
+        A.OpticalDistortion(p=0.3),
+        A.GridDistortion(p=0.1),
+        A.IAAPiecewiseAffine(p=0.3),
+    ], p=0.2),
+    A.OneOf([
+        A.IAASharpen(),
+        A.IAAEmboss(),
+        A.RandomBrightnessContrast(),
+    ], p=0.3),
+    A.HueSaturationValue(p=0.3),
+    A.ToFloat()
 ])
+
+
+img = cv2.imread('../data/Train/A/A002.png', 1)
+img = cv2.resize(img, (224, 224))
+
+aug_data = train_transforms(image=img)
+print(np.max(aug_data['image']))
