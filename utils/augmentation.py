8d3ab170e603c31f75dc81053cd8ce0c8ae72765
omerferhatt <omerf.sarioglu@gmail.com>
2021-01-04 02:48:53 +0300

    Code update and model change - File write added

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -4,7 +4,7 @@ import albumentations as A
 
 
 train_transforms = A.Compose([
-    A.Rotate((-20, 20), always_apply=True),
+    A.Rotate((-15, 15), always_apply=True),
     A.HorizontalFlip(p=0.5),
     A.OneOf([
         A.IAAAdditiveGaussianNoise(),
@@ -15,7 +15,7 @@ train_transforms = A.Compose([
         A.MedianBlur(blur_limit=3, p=0.1),
         A.Blur(blur_limit=3, p=0.1),
     ], p=0.2),
-    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
+    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=10, p=0.2),
     A.OneOf([
         A.OpticalDistortion(p=0.3),
         A.GridDistortion(p=0.1),
