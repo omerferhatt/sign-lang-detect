88c82747f23c426bae925187684b141e774c0b4e
omerferhatt <omerf.sarioglu@gmail.com>
2021-01-18 22:22:51 +0300

    Code update and model change - Confusion matrix added

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix
 
 from utils.dataset import Dataset
@@ -20,7 +21,28 @@ _, y = next(test_ds.take(1).as_numpy_iterator())
 y = np.argmax(y, axis=-1)
 
 print('Confusion Matrix')
-print(confusion_matrix(y, y_pred))
+conf_res = confusion_matrix(y, y_pred)
+print(conf_res)
 print('Classification Report')
 target_names = dataset_test.labels
 print(classification_report(y, y_pred, target_names=target_names))
+
+target_names[-2] = 'DEL'
+target_names[-1] = 'SPACE'
+fig, ax = plt.subplots(figsize=(10, 15))
+im = ax.imshow(conf_res)
+ax.set_xticks(np.arange(len(target_names)))
+ax.set_yticks(np.arange(len(target_names)))
+# ... and label them with the respective list entries
+ax.set_xticklabels(target_names)
+ax.set_yticklabels(target_names)
+plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
+         rotation_mode="anchor")
+
+for i in range(len(target_names)):
+    for j in range(len(target_names)):
+        text = ax.text(j, i, conf_res[i, j],
+                       ha="center", va="center", color="w")
+
+fig.tight_layout()
+plt.show()
